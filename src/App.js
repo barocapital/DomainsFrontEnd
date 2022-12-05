7b08a4d16cbc10b04f70cecfe46423afa59e8fd0
samuelpolino97 <polinosamuel1997@outlook.com>
2022-12-05 15:59:08 -0600

    QRCODE BASE64

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WalletCard from "./WalletCard";
+import WalletCardEthers from "./WalletCardEthers";
+
+function App() {
+  return (
+    <div className="App">
+      <WalletCard />
+    </div>
+  );
+}
+
+export default App;
