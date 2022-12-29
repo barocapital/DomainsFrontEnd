6a86fef18d5e1408862f319d1a332037707ae553
samuelpolino97 <polinosamuel1997@outlook.com>
2022-12-28 19:37:55 -0600

    New UI and features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 import WalletCard from "./WalletCard";
-import {Profile} from "./Profile";
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { publicProvider } from 'wagmi/providers/public'
-import { Provider,createClient,configureChains,useAccount, useConnect, useDisconnect } from 'wagmi'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { Chain } from 'wagmi'
-import { useNetwork } from 'wagmi'
-import { polygon} from 'wagmi/chains'
-import { WagmiConfig } from 'wagmi'
-import {polygonMainnet,polygonTestnet} from './methods/Chains.jsx'
-
+import { Profile } from "./Profile";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { publicProvider } from "wagmi/providers/public";
+import {
+  Provider,
+  createClient,
+  configureChains,
+  useAccount,
+  useConnect,
+  useDisconnect,
+} from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { Chain } from "wagmi";
+import { useNetwork } from "wagmi";
+import { polygon } from "wagmi/chains";
+import { WagmiConfig } from "wagmi";
+import { polygonMainnet, polygonTestnet } from "./methods/Chains.jsx";
 
 const { chains, provider } = configureChains(
   [polygonTestnet],
@@ -27,37 +33,22 @@ const { chains, provider } = configureChains(
   ]
 );
 
-
 const client = createClient({
   autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-  ],
-  provider:provider
-})
+  connectors: [new MetaMaskConnector({ chains })],
+  provider: provider,
+});
 
- 
 export function App() {
-  const { address, connector, isConnected } = useAccount()
-  return(
+  const { address, connector, isConnected } = useAccount();
+  return (
     <center>
-    <WagmiConfig client={client}>
-        <Profile/>
-          { 
-            isConnected==true?
-            <>
-              <WalletCard/>
-            </>:
-            <>
-            </>
-          }
-        
+      <WagmiConfig client={client}>
+        <Profile />
+        {isConnected == true ? <></> : <></>}
       </WagmiConfig>
     </center>
-  )
-   
+  );
 }
-
-
 
 export default App;
